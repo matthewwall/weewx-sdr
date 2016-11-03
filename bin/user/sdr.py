036e4496e8f2490fd811fda9c9adcaea00bbc385
Matthew Wall <mwall@users.sourceforge.net>
2016-11-03 15:10:29 -0400

    added rudimentary filtering for packet capture

--- bin/user/sdr.py
+++ bin/user/sdr.py
@@ -845,6 +845,8 @@ if __name__ == '__main__':
                       help='value for PATH')
     parser.add_option('--ld_library_path', dest='ld_library_path',
                       help='value for LD_LIBRARY_PATH')
+    parser.add_option('--filter', dest='filter', default='',
+                      help='output to be hidden: out, parsed, unparsed')
     parser.add_option('--action', dest='action', default='show-packets',
                       help='actions include show-packets, show-detected, list-supported')
 
@@ -880,15 +882,19 @@ if __name__ == '__main__':
             print detected
     else:
         # display output and parsed/unparsed packets
+        hidden = [x.strip() for x in options.filter.split(',')]
         mgr = ProcManager()
         mgr.startup(options.cmd, path=options.path,
                     ld_library_path=options.ld_library_path)
         for lines in mgr.get_stdout():
-            print "out:", lines
+            if 'out' not in hidden:
+                print "out:", lines
             packet = PacketFactory.create(lines)
             if packet:
-                print 'parsed: %s' % packet
+                if 'parsed' not in hidden:
+                    print 'parsed: %s' % packet
             else:
-                print "unparsed:", lines
+                if 'unparsed' not in hidden:
+                    print "unparsed:", lines
         for lines in mgr.get_stderr():
             print "err:", lines
