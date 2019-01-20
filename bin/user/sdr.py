68c066433b21c7d1cacd9f3d46ddae1e61244f3a
Matthew Wall <matthewwall@users.noreply.github.com>
2019-01-20 15:37:14 -0500

    added alecto rain packet

--- bin/user/sdr.py
+++ bin/user/sdr.py
@@ -90,7 +90,7 @@ from weeutil.weeutil import tobool
 
 
 DRIVER_NAME = 'SDR'
-DRIVER_VERSION = '0.51'
+DRIVER_VERSION = '0.53'
 
 # The default command requests json output from every decoder
 # -q - suppress non-data messages (for older versions of rtl_433)
@@ -721,6 +721,24 @@ class AlectoV1WindPacket(Packet):
          pkt['battery'] = 0 if obj.get('battery') == 'OK' else 1
          pkt['channel'] = obj.get('channel')
          pkt = Packet.add_identifiers(pkt, station_id, AlectoV1WindPacket.__name__)
+         return pkt
+
+
+class AlectoV1RainPacket(Packet):
+    # {"time" : "2019-01-20 15:29:21", "model" : "AlectoV1 Rain Sensor", "id" : 13, "channel" : 0, "battery" : "OK", "rain_total" : 15.500, "mic" : "CHECKSUM"}
+
+    IDENTIFIER = "AlectoV1 Rain Sensor"
+
+    @staticmethod
+    def parse_json(obj):
+         pkt = dict()
+         pkt['dateTime'] = Packet.parse_time(obj.get('time'))
+         pkt['usUnits'] = weewx.METRIC # FIXME: units have not been verified
+         station_id = obj.get('id')
+         pkt['rain_total'] = Packet.get_float(obj, 'rain_total')
+         pkt['battery'] = 0 if obj.get('battery') == 'OK' else 1
+         pkt['channel'] = obj.get('channel')
+         pkt = Packet.add_identifiers(pkt, station_id, AlectoV1RainPacket.__name__)
          return pkt
 
 
@@ -1894,6 +1912,7 @@ class PacketFactory(object):
         AcuriteWT450Packet,
         AlectoV1TemperaturePacket,
         AlectoV1WindPacket,
+        AlectoV1RainPacket,
         AmbientF007THPacket,
         Bresser5in1Packet,
         CalibeurRF104Packet,
--- install.py
+++ install.py
@@ -10,7 +10,7 @@ def loader():
 class SDRInstaller(ExtensionInstaller):
     def __init__(self):
         super(SDRInstaller, self).__init__(
-            version="0.52",
+            version="0.53",
             name='sdr',
             description='Capture data from rtl_433',
             author="Matthew Wall",
