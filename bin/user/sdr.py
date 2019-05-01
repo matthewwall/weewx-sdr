172e487265d0f2bd9e98b76d6f953caa1e637d98
Matthew Wall <matthewwall@users.noreply.github.com>
2019-05-01 12:04:00 -0400

    added support for OS TH128

--- bin/user/sdr.py
+++ bin/user/sdr.py
@@ -90,7 +90,7 @@ from weeutil.weeutil import tobool
 
 
 DRIVER_NAME = 'SDR'
-DRIVER_VERSION = '0.63.1'
+DRIVER_VERSION = '0.64'
 
 # The default command requests json output from every decoder
 # -q - suppress non-data messages (for older versions of rtl_433)
