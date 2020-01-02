655487f3cb66e0bf96425ebf0d1183d889079640
Matthew Wall <matthewwall@users.noreply.github.com>
2020-01-02 16:07:31 -0500

    use weecfg.extension not setup

--- install.py
+++ install.py
@@ -2,7 +2,7 @@
 # Copyright 2016-2019 Matthew Wall
 # Distributed under the terms of the GNU Public License (GPLv3)
 
-from setup import ExtensionInstaller
+from weecfg.extension import ExtensionInstaller
 
 def loader():
     return SDRInstaller()
